c1b897b77101c6684f0ebcce3a1996dc5df29390
barbie77777 <00001120057425sp@al.educacao.sp.gov.br>
2024-10-10 09:00:21 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Qual o maior planeta do sistema solar?',
+    'Jupiter'
+)
+
+criaCartao(
+    'Gostos',
+    'Qual a matéria que você mais gosta?',
+    'Biologia'
+)
+
+criaCartao(
+    'CURIOSIDADE',
+    'Cantor favorito?',
+    'Jorge e Mateus'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Fsço curso de ?',
+    'Espanhol'
+)
